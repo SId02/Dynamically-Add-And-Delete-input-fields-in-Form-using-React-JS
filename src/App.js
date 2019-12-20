cf42bc6e2611e1bd01da6324babd2b0dcd8e7fd9
Siddhesh M <31335947+SId02@users.noreply.github.com>
2019-12-20 15:49:12 +0530

    i
    i

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BookView from './components/bookView';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BookView />
+      </header>
+    </div>
+  );
+}
+
+export default App;
